850714bb58319ac7505121dc5e7bc42f9e7fdaa7
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2021-09-23 14:58:59 +1200

    add simple chatbot page with codeblock

--- chatbot/src/components/pages/chatbot/restaurantChatbotPage.js
+++ chatbot/src/components/pages/chatbot/restaurantChatbotPage.js
@@ -2,6 +2,8 @@ import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import {useParams} from "react-router-dom";
 import { useAuth} from "../../../context/AuthContext";
+import IntegrationCodeBlock from "./integrationCodeBlock"
+import { Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
 
@@ -25,12 +27,15 @@ export default function RestaurantChatbotPage() {
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return(
     <div>
+    <Paper elevation={3}>
     <p>Chatbot for restaurant: {id}</p>
+    <p >instruction for integration......</p>
+    </Paper>
+    <IntegrationCodeBlock chatPath={restaurantChatbotPath} />
     <df-messenger
       intent="WELCOME"
       chat-title="test-bot"
       agent-id="726c85cc-ff28-494b-baaf-e439a6e65d09"
-      expand="true"
       user-id={restaurantChatbotPath}
       language-code="en"
     ></df-messenger>
