129ea31fea7e3389f6374c8220d0b2d63fbcab63
mrknight21 <30679549+mrknight21@users.noreply.github.com>
2021-09-23 12:01:06 +1200

    added chatbot for restaurant chatbot page

--- chatbot/src/components/pages/chatbot/restaurantChatbotPage.js
+++ chatbot/src/components/pages/chatbot/restaurantChatbotPage.js
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import {useParams} from "react-router-dom";
+import { useAuth} from "../../../context/AuthContext";
 
 const useStyles = makeStyles((theme) => ({
 
@@ -18,10 +19,22 @@ const useStyles = makeStyles((theme) => ({
 
 export default function RestaurantChatbotPage() {
   let { id } = useParams();
-  const restaurantChatbotPath = ""
+  const {currentUser} = useAuth()
+  const restaurantChatbotPath = currentUser.uid + "/" + id;
   const classes = useStyles();
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
   return(
-    <p>Order for restaurant: {id}</p>
+    <div>
+    <p>Chatbot for restaurant: {id}</p>
+    <df-messenger
+      intent="WELCOME"
+      chat-title="test-bot"
+      agent-id="726c85cc-ff28-494b-baaf-e439a6e65d09"
+      expand="true"
+      user-id={restaurantChatbotPath}
+      language-code="en"
+    ></df-messenger>
+    </div>
+    
   );
 }
